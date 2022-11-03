4925a84a27284d371fda3937608aba6bf54fcc78
OhMyMy426 <116591324+OhMyMy426@users.noreply.github.com>
2022-11-03 21:18:48 +0100

    new comments

--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -1,22 +1,54 @@
+/*
+ * ParticleContainer.h
+ *
+ *  Created on: 30.10.2022
+ *      Author: Team-B
+ */
+
 #pragma once
 #include <unordered_map>
 
 
 class ParticleContainer {
 private:
-
+        /**
+	* the container of the particle with the type unordered_map<int, Particle>
+	*/
 	std::unordered_map<int, Particle> Particles;
-    int particle_counter = 0;
+	
+	/**
+	* number of the particle
+	*/
+	int particle_counter = 0;
 
 public:
+	/**
+	* the getter-function for the particle_counter
+	*/
 	const int getParticle_counter() const;
 
-	std::unordered_map<int, Particle> &getParticles();
+	/**
+	* the getter-function for the container of the particle
+	*/
+	std::unordered_map<int, Particle>& getParticles();
 
-	ParticleContainer(std::unordered_map <int, Particle>&  Particles_arg, int& particle_counter_arg);
+	/**
+	* one constructor for the class ParticleContainer with parameter-type unordered_map <int, Particle>& and int&
+	*/
+	ParticleContainer(std::unordered_map <int, Particle>& Particles_arg, int& particle_counter_arg);
 
+	/**
+	* another constructor for the class ParticleContainer with parameter-type ParticleContainer&
+	*/
 	ParticleContainer(const ParticleContainer& other);
-	virtual ~ParticleContainer();
-    void setParticleCounter(int x);
 
+	/**
+	* destructor for the class ParticleContainer
+	*/
+	virtual ~ParticleContainer();
+
+	/**
+	* a setter-function for the class ParticleContainer with parameter-type int
+	*/
+	void setParticleCounter(int x);
 };
