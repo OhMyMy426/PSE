915d546d1d071c1b411b019ca9af4535cd38d5ea
XStyxX <88464346+XStyxX@users.noreply.github.com>
2022-11-14 14:13:14 +0100

    Update Generator.cpp

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -3,17 +3,33 @@
 #include "Generator.h"
 #include "Cuboid.h"
 #include <array>
+#include <iostream>
 
 
 Generator::Generator(const Generator& other) {
     Particles = other.Particles;
 }
 
-Generator::Generator(std::vector<Cuboid> Cuboids) {
+Generator::Generator(std::vector<Cuboid> Cuboids_args) :Cuboids(Cuboids_args) {
+    std::size_t size = { 0 };
     for (Cuboid n : Cuboids) {
         int x_size = n.getAmountOfParticles().at(0);
         int y_size = n.getAmountOfParticles().at(1);
         int z_size = n.getAmountOfParticles().at(2);
+        size += x_size * y_size * z_size;
+    }
+    Particles.reserve(size);
+    std::cout << "Cuboids are already stored!" << std::endl;
+}
+
+
+void initialise(std::vector<Cuboid>& cuboids, std::unordered_map<int, Particle>& particles, int& particleCounter) {
+    for (Cuboid n : cuboids) {
+        int x_size = n.getAmountOfParticles().at(0);
+        int y_size = n.getAmountOfParticles().at(1);
+        int z_size = n.getAmountOfParticles().at(2);
+        particleCounter = x_size * y_size * z_size;
+        int map_counter = { 0 };
         for (int i{ 0 };i < x_size;++i) {
             for (int j{ 0 };i < x_size;++i) {
                 for (int k{ 0 };i < x_size;++i) {
@@ -21,11 +37,18 @@ Generator::Generator(std::vector<Cuboid> Cuboids) {
                     position.at(0) += i * n.getMeshWidth();
                     position.at(1) += j * n.getMeshWidth();
                     position.at(2) += k * n.getMeshWidth();
-                
-                    Particles.push_back(Particle(position, n.getInitialVelocity(), n.getParticleMass(), 0)); //Changes here
+                    particles.emplace(map_counter, (Particle(position, n.getInitialVelocity(), n.getParticleMass(), 0)));
+                    map_counter++;
                 }
             }
         }
     }
 }
 
+
+std::vector<Cuboid>& Generator::getCuboids() {
+    return Cuboids;
+};
+std::vector<Particle>& Generator::getParticles() {
+    return Particles;
+}
