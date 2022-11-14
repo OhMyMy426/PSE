ad5b6d3e0a2a59c5231eff350aa6c00564ee2a47
XStyxX <88464346+XStyxX@users.noreply.github.com>
2022-11-14 14:12:39 +0100

    changed Generator to main-status

--- src/Generator.h
+++ src/Generator.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <vector>
 #include <unordered_map>
-#include "Particle.h"
-#include "Cuboid.h"
+#include <Particle.h>
+#include <Cuboid.h>
 
 class Generator
 {
 private:
+    std::vector<Cuboid> Cuboids;
     std::vector<Particle> Particles;
 public:
     Generator(const Generator& other);
     Generator(std::vector<Cuboid> cuboids);
+    void initialise(std::vector<Cuboid>& cuboids, std::unordered_map<int,Particle>& particles, int& particleCounter);
+    std::vector<Cuboid>& getCuboids();
+    std::vector<Particle>& getParticles();
 };
 
 
