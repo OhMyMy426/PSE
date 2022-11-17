b3d848cd35b256f70be99776c7210d5abad95a97
ga84qof <116753331+ga84qof@users.noreply.github.com>
2022-11-17 12:35:53 +0100

    spdlog fix

--- src/first_test.cc
+++ src/first_test.cc
@@ -3,6 +3,11 @@
 #include "Particle.cpp"
 #include <unordered_map>
 
+//#include"Calculator.h"
+#include"LJCalculator.h"
+#include"Particle.h"
+
+
 void Factorial(int n, int& result)
 {
   result = 1;
@@ -33,3 +38,67 @@ TEST(ParticaleContainerTest, Easy_Test) {
   static ParticleContainer particleContainer(particles, particle_counter);
   EXPECT_EQ(20, particleContainer.getParticle_counter());
 }
+
+/*TEST(CalculateFTest, Easy_Test3) {
+    LJCalculator calculator;
+     double sigma = 1.0;
+    double epsilon = 0.0;
+
+std::array<double, 3> f= {0,0,0};
+   std::array<double, 3> v= {0,0,0};
+   std::array<double, 3> x= {0,0,0};
+   std::array<double, 3> x1= {1,1,1};
+  std::array<double, 3> f1= {1,1,1};
+
+  Particle Particle2(x1,x1,1,1);
+  Particle Particle(x,x,1,1); 
+  
+   
+    
+   
+   
+   Particle2.setF(f1);
+   Particle2.setX(x1);
+   Particle2.setV(v);
+  Particle.setF(f);
+  Particle.setX(x);
+  Particle.setV(v);
+  
+ 
+   calculator.calculateF_LJ(Particle,Particle, sigma, epsilon);
+  EXPECT_EQ(f, Particle.getF());
+}*/
+
+/*TEST(CalculatexTest, Easy_Test4) {
+  std::unordered_map<int, Particle> particles;
+  int particle_counter4 = 2;
+  static ParticleContainer particleContainer(particles, particle_counter4);
+    std::array<double, 3> x= {0,0,0};
+  particleContainer.getParticles().at(0).setF(  x);
+  particleContainer.getParticles().at(0).setX(x);
+  particleContainer.getParticles().at(0).setv(x);
+  particleContainer.getParticles().at(0).setOldF(x);
+   particleContainer.getParticles().at(0).setM(20);
+   cal
+
+
+  calculateX(particleContainer,0.2);
+  
+EXPECT_EQ(x, particleContainer.getParticles().at(0).getX());
+}
+
+TEST(CalculateVTest, Easy_Test5) {
+  std::unordered_map<int, Particle> particles;
+  int particle_counter5 = 2;
+  static ParticleContainer particleContainer(particles, particle_counter5);
+  std::array<double, 3> v= {0,0,0};
+  particleContainer.getParticles().at(0).setF(  v);
+  particleContainer.getParticles().at(0).setX(v);
+  particleContainer.getParticles().at(0).setv(v);
+  particleContainer.getParticles().at(0).setOldF(v);
+   particleContainer.getParticles().at(0).setM(20);
+  calculateV(particleContainer,0.2);
+    
+EXPECT_EQ(v, particleContainer.getParticles().at(0).getV());
+}
+*/
