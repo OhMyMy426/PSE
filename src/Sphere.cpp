29368b0c660e82762826641a8f55f7af275d13f7
XStyxX <ge52kij@mytum.de>
2022-11-24 02:03:33 +0100

    added Sphere. here, the Dimension is IMPORTANT for the simulation

---
+++ src/Sphere.cpp
@@ -0,0 +1,5 @@
+//
+// Created by A_M_S on 23.11.2022.
+//
+
+#include "Sphere.h"
---
+++ src/Sphere.h
@@ -0,0 +1,16 @@
+//
+// Created by A_M_S on 23.11.2022.
+//
+
+#ifndef REALPSE_SPHERE_H
+#define REALPSE_SPHERE_H
+
+
+
+class Sphere {
+
+};
+
+
+
+#endif //REALPSE_SPHERE_H
